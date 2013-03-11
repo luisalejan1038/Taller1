e5c1d8ddd12387c5d3714eceb3f2d9eb7b437145
unknown <luis.silva@VISUALSALE2.mind.com.co>
2013-03-11 14:09:59 -0500

    Correccion Taller1

--- for.sh
+++ for.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+
+find . -name "*.txt" > PbLista.txt
+
 for line in $(cat PbLista.txt)
    do 
-     grep -i "\(D\)\(B\)" $line | cat >> PbLista2.txt  
-done  
+      val=`grep -c "\(DB\)" $line`
+      echo "$val"
+      if [ $val -ne 0 ] 
+       then 
+        cat $line >> PbLista2.txt
+      fi            
+done
+
+
+
+
+
+
+
+
